7b03e618269e70df2cbd5e3f78721d0903caf984
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-26 19:05:11 -0300

    app and useState component creation

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// import { HooksApp } from './HooksApp'
+import { TrafficLight } from './01-useState/TrafficLight'
+
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* <HooksApp /> */}
+    <TrafficLight />
+  </StrictMode>,
+)
