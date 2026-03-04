5150cc0a34921bba741a924b5d0540a3b4517644
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-04 20:35:50 -0300

    UserContext first steps

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,3 +1,48 @@
-export const UserContext = () => {
-  return <div>UserContext</div>
+import { createContext, useState, type PropsWithChildren } from 'react'
+import type { User } from '../data/user-moc.data'
+
+// interface UserContextProps {
+//   children: React.ReactNode
+// }
+
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated'
+
+interface UserContextProps {
+  // state
+  authStatus: AuthStatus
+  user: User | null
+
+  // Methods
+  login: (userId: number) => boolean
+  logout: () => void
+}
+
+export const UserContext = createContext({} as UserContextProps)
+
+// HOC
+export const UserContextProvider = ({ children }: PropsWithChildren) => {
+  const [authStatus, setAuthStatus] = useState<AuthStatus>('checking')
+  const [user, setUser] = useState<User | null>(null)
+
+  const handleLogin = (userId: number) => {
+    console.log({ userId })
+    return true
+  }
+
+  const handleLogout = () => {
+    console.log('logout')
+  }
+
+  return (
+    <UserContext
+      value={{
+        authStatus: authStatus,
+        user: user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </UserContext>
+  )
 }
